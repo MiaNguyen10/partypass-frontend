f7c8b721be4fc730c1f7eaec36e4f99ecd55231c
MiaNguyen10 <hanguyendahlia@gmail.com>
2024-11-24 01:50:13 +0200

    deploy config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,22 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  base: "/partypass-frontend/",
+  build: {
+    chunkSizeWarningLimit: 1000,
+    minify: "terser", // Use terser for better tree-shaking
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          mui: ["@mui/material", "@mui/icons-material", "@mui/x-date-pickers"],
+          react: ["react", "react-dom", "react-router-dom"],
+          redux: ["@reduxjs/toolkit", "react-redux"],
+          leaflet: ["leaflet", "react-leaflet"],
+        },
+      },
+    },
+  },
+});
