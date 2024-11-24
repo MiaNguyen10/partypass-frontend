e0f0567dc0b969352228041520a56290e10cf421
MiaNguyen10 <hanguyendahlia@gmail.com>
2024-11-24 12:37:25 +0200

    update react router dom, adjust vite config

--- src/app/middlewares/PermissionProvider/PermissionContext.jsx
+++ src/app/middlewares/PermissionProvider/PermissionContext.jsx
@@ -1,5 +1,3 @@
 import { createContext } from "react";
 
-const PermissionContext = createContext();
-
-export { PermissionContext };
+export const PermissionContext = createContext(null);
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: "/partypass-frontend/",
+  base: mode === "production" ? "/partypass-frontend/" : "/", // Set base dynamically
   build: {
     chunkSizeWarningLimit: 1000,
     minify: "terser", // Use terser for better tree-shaking
@@ -18,4 +18,4 @@ export default defineConfig({
       },
     },
   },
-});
+}));
