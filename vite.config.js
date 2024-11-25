7c00254eb43ddf35ef8b2bc20e6dfe847f2b8e45
MiaNguyen10 <hanguyendahlia@gmail.com>
2024-11-25 16:51:08 +0200

    deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   plugins: [react()],
-  base: mode === "production" ? "/partypass-frontend/" : "/", // Set base dynamically
+  base: mode === "production" ? "/partypass-frontend/" : "/",
   build: {
     chunkSizeWarningLimit: 1000,
-    minify: "terser", // Use terser for better tree-shaking
+    minify: "terser",
     rollupOptions: {
       output: {
-        manualChunks: {
-          mui: ["@mui/material", "@mui/icons-material", "@mui/x-date-pickers"],
-          react: ["react", "react-dom", "react-router-dom"],
-          redux: ["@reduxjs/toolkit", "react-redux"],
+        manualChunks: (id) => {
+          if (id.includes("node_modules")) {
+            return id
+              .toString()
+              .split("node_modules/")[1]
+              .split("/")[0]
+              .toString();
+          }
         },
       },
     },
