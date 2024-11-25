0433a848b2e12ac10e0a4d155369c290fed52a4e
MiaNguyen10 <hanguyendahlia@gmail.com>
2024-11-25 17:11:38 +0200

    update deploy to netlify

--- vite.config.js
+++ vite.config.js
@@ -1,24 +1,28 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-export default defineConfig(({ mode }) => ({
-  plugins: [react()],
-  base: mode === "production" ? "/partypass-frontend/" : "/",
-  build: {
-    chunkSizeWarningLimit: 1000,
-    minify: "terser",
-    rollupOptions: {
-      output: {
-        manualChunks: (id) => {
-          if (id.includes("node_modules")) {
-            return id
-              .toString()
-              .split("node_modules/")[1]
-              .split("/")[0]
-              .toString();
-          }
+export default defineConfig(() => {
+  // eslint-disable-next-line no-undef
+  const isGitHubPages = process.env.DEPLOY_PLATFORM === "github";
+  return {
+    plugins: [react()],
+    base: isGitHubPages ? "/partypass-frontend/" : "/", // Use `/` for Netlify and `/repo-name/` for GitHub Pages
+    build: {
+      chunkSizeWarningLimit: 1000,
+      minify: "terser",
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes("node_modules")) {
+              return id
+                .toString()
+                .split("node_modules/")[1]
+                .split("/")[0]
+                .toString();
+            }
+          },
         },
       },
     },
-  },
-}));
+  };
+});
